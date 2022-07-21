b6ca0e2298a87191710722d1e8759949139bb6e0
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-21 20:46:56 +0200

    implementato anche il metodo delete

--- app/Http/Controllers/Admin/CategoryContoller.php
+++ app/Http/Controllers/Admin/CategoryContoller.php
@@ -94,8 +94,10 @@ class CategoryContoller extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return redirect()->route('admin.categories.index');
     }
 }
